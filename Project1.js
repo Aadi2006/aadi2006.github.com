56dd6d09187ac783a5ddf561175f90bc443808e4
Aadi <70697442+Aadi2006@users.noreply.github.com>
2020-11-16 00:24:23 +0530

    Update Project1.js

--- Project1.js
+++ Project1.js
@@ -1,2 +1,8 @@
 var myname="Aadi";
 document.getElementById("name").innerHTML = "My name is "+myname;
+sen="";
+food=["Pizza","dominos","burger","kfc"];
+for(i=0,i<food.length,i++){
+sen=sen+"\n"+food[i];
+}
+document.getElementByid("se").innerHTML=sen;
